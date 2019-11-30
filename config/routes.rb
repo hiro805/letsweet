592d8b4679174dcc60629139157e392241ea7e26
Hiroshi <ionazun0805@gmail.com>
2019-12-01 01:50:11 +0900

    create_my_page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
  root 'tweets#index'
  get 'tweets/new' => 'tweets#new'
- post 'tweets'    => 'tweets#create'
+ post 'tweets' => 'tweets#create'
+ get 'users/:id' => 'users#show'
 end
